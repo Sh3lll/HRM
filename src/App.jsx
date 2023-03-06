3672a64398d318f73528a912f10c61b5d93df5f7
Joe Isaac <isaacjoseph950@gmail.com>
2023-03-06 08:39:32 +0300

    added docker files, yet to test

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,80 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+//   function check(arr){
+//     if(arr.length % 2 !== 0 ){
+//         console.log("cannot do op on uneven number");
+//                     return
+//     }
+  
+
+// }
+
+const check = (expression) => {
+  const stack = [];
+  const bracketLookup = {
+    '{': '}',
+    '(': ')',
+    '[': ']',
+  };
+
+  for (const key of expression) {
+    if(Object.keys(bracketLookup).includes(key)) { // matches open brackets
+      stack.push(key);
+    } else if(Object.values(bracketLookup).includes(key)) { //matches closed brackets
+      const lastBracket = stack.pop();
+      if(bracketLookup[lastBracket] !== key) {
+        return false;
+      }
+
+    }
+  }
+  return stack.length === 0;
+}
+
+
+// function check(arr){
+//     //Assuming it's already an array
+//     //the goal is to find matching consecutive symbols
+
+//     const expected = {
+//       "{" : "}",
+//       "(" : ")",
+//       "[" : "]"
+//     }
+
+//     let stack = [];
+
+//     for(let i = 0; i<arr.length-1; i++){
+//       if(Object.keys(expected).includes(arr[i])){
+//         stack.push(arr[i]);
+//       }
+//       else if(Object.values(expected).includes(arr[i])){
+//         let i = stack.pop();
+//         if( i === arr[i]){
+//           return true;
+//         }
+//         return false
+//       }
+//     }
+// }
+
+let myArr = ["(", ")", "{", "}"];
+
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => {setCount((count) => count + 1); console.log(check(myArr));}}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
